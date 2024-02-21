b57b4a9e2e5e4cdddfcf381fac6b530302cc0940
Lee-Jun-Guk <ell0404@naver.com>
2024-02-21 14:40:53 +0900

    jenkins test2

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -14,7 +14,7 @@
                 </a>
                 <ul class="topMenu">
                     <li v-show="!isLoggedIn">
-                        <a href="/signup/member">가입</a>
+                        <a href="/signup/member">가입해봐라</a>
                     </li>
                     <li v-show="!isLoggedIn">
                         <a href="/login/member">로그인</a>
